eaed7949adbf94c6c81d1b42d00150a15845ece6
Craig Bowditch <craigbowditch@hotmail.com>
2018-09-30 19:40:26 +0100

    creates hangar class with tests.

--- src/main/java/Hangar.java
+++ src/main/java/Hangar.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class Hangar {
+
+    private ArrayList<Plane> planes;
+    private String name;
+
+    public Hangar(String name) {
+        this.planes = new ArrayList<>();
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int planeCount() {
+        return this.planes.size();
+    }
+
+    public void addPlane(Plane plane) {
+        this.planes.add(plane);
+    }
+
+
+    public Plane removePlane(Plane plane) {
+        return this.planes.remove(this.planes.indexOf(plane));
+
+    }
 }
--- src/test/java/HangarTest.java
+++ src/test/java/HangarTest.java
@@ -1,2 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HangarTest {
+
+    private Hangar hangar;
+    private Plane plane;
+
+    @Before
+    public void before(){
+        hangar = new Hangar("hangar 1");
+        plane = new Plane(PlaneType.BOEING747, Airline.BRITISHAIRWAYS);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("hangar 1", hangar.getName());
+    }
+
+    @Test
+    public void startsWithNoPlanes() {
+        assertEquals(0, hangar.planeCount());
+    }
+
+    @Test
+    public void canAddPlaneToHangar() {
+        hangar.addPlane(plane);
+        assertEquals(1, hangar.planeCount());
+    }
+
+    @Test
+    public void canRemovePlaneFromHangar() {
+        hangar.addPlane(plane);
+        hangar.addPlane(plane);
+        assertEquals(2, hangar.planeCount());
+        hangar.removePlane(plane);
+        assertEquals(1, hangar.planeCount());
+    }
 }
