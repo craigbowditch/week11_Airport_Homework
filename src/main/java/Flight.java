2b0f17d62a6e69f0d8776a1be8a000c4413b73d5
Craig Bowditch <craigbowditch@hotmail.com>
2018-09-30 15:51:54 +0100

    creates flight class with tests

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,24 @@
 public class Flight {
+
+    private Plane plane;
+    private String flightNumber;
+    private AirportCode destination;
+
+    public Flight(Plane plane, String flightNumber, AirportCode destination) {
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+    }
+
+    public Plane getPlane() {
+        return this.plane;
+    }
+
+    public String getFlightNumber() {
+        return this.flightNumber;
+    }
+
+    public AirportCode getDestination() {
+        return destination;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    private Flight flight;
+    private Plane plane;
+
+    @Before
+    public void before(){
+        plane = new Plane(PlaneType.BOEING747, Airline.BRITISHAIRWAYS);
+        flight = new Flight(plane, "BA99", AirportCode.JFK);
+    }
+
+    @Test
+    public void hasPlane() {
+        assertEquals(plane, flight.getPlane());
+    }
+
+    @Test
+    public void hasFlightNumber() {
+        assertEquals("BA99", flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination() {
+        assertEquals(AirportCode.JFK, flight.getDestination());
+    }
 }
