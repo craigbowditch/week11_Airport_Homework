dd83078db7594371672f07fc133556adfa6ebc1e
Craig Bowditch <craigbowditch@hotmail.com>
2018-09-30 15:10:06 +0100

    creates plane class with tests.

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class Plane {
+
+    private PlaneType planeType;
+    private Airline airline;
+    private ArrayList<Passenger> passengers;
+
+    public Plane(PlaneType planeType, Airline airline) {
+        this.planeType = planeType;
+        this.airline = airline;
+        this.passengers = new ArrayList<>();
+    }
+
+    public PlaneType getPlaneType() {
+        return planeType;
+    }
+
+    public Airline getAirline() {
+        return airline;
+    }
+
+    public int getCapacity() {
+        return planeType.getCapacity();
+    }
+
+    public int passengerCount() {
+        return this.passengers.size();
+    }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+   private Plane plane;
+
+   @Before
+    public void before(){
+       plane = new Plane(PlaneType.BOEING747, Airline.BRITISHAIRWAYS);
+   }
+
+    @Test
+    public void hasPlaneType() {
+       assertEquals(PlaneType.BOEING747, plane.getPlaneType());
+    }
+
+    @Test
+    public void hasAirline() {
+       assertEquals(Airline.BRITISHAIRWAYS, plane.getAirline());
+    }
+
+    @Test
+    public void hasCapacity() {
+       assertEquals(416, plane.getCapacity());
+    }
+
+    @Test
+    public void startsWithNoPassengers() {
+       assertEquals(0, plane.passengerCount());
+    }
 }
